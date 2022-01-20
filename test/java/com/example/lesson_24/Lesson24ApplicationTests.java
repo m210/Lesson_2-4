670aa3bd186b57208f2c79605b29aa515ea1106a
[M210®] <m210-2007@mail.ru>
2022-01-20 13:30:16 +0300

    Initial commit

---
+++ test/java/com/example/lesson_24/Lesson24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
